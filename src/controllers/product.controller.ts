16882650a3322d864316491b33a7a86258fd2bbe
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-07-05 11:10:00 +0530

    update product.controller.ts

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+// controller functions to handle get all products request
+
+export const getAllProducts = (req : Request, res: Response) => {
+}
+
+export const saveProduct = (req : Request, res: Response) => {
+}
+
+export const getProductById =(req : Request, res: Response) => {
+}
+
+export const updateProduct = (req : Request, res: Response) => {
+}
+
+export const deleteProduct = (req : Request, res: Response) => {
+}
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,11 +1,22 @@
 import { Router } from "express"
+import {
+    deleteProduct,
+    getAllProducts,
+    getProductById,
+    saveProduct,
+    updateProduct
+} from "../controllers/product.controller";
 
 
 const productRoutes: Router = Router();
 
  // handle incoming requests
 
-productRoutes.get("/all",); // get all products
-productRoutes.post("/save",); // save a new product
+productRoutes.get("/all",getAllProducts); // get all products
+productRoutes.get("/:id",getProductById); // get a product
+productRoutes.post("/save",saveProduct); // save a new product
+productRoutes.put("/update/:id",updateProduct); // update a product
+productRoutes.delete("/delete/:id",deleteProduct); // delete a product
+
 
 export default productRoutes;
