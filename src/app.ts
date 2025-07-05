ed5caa667240cec901f76afe5abfc19c58d31c13
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-07-05 17:05:36 +0530

    node modules

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, {Express} from "express";
 import productRoutes from "./routes/product.routes";
+import cors from "cors";
 
 const app: Express = express();
 
@@ -7,7 +8,19 @@ const app: Express = express();
 export default app;
 
 // 2 Define Middleware
-    // 2.1 instruct to parse the request payload data to be converted to JSON format
+// 2.1 instruct to parse the request payload data to be converted to JSON format
 
 app.use(express.json());
+//app.use(cors()) // enable/allow CORS here
+const allowedOrigins = ["http://localhost:5173"];
+
+const crsOptions = {
+    origin: (origin: string | undefined, callback: (error: Error | null, allow?: boolean) => void) => {
+        if (!origin || allowedOrigins.includes(origin)) { // postman request allowed
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+}
 app.use("/api/products", productRoutes);
