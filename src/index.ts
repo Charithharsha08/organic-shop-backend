866e38591f89536dfe19e6beb50e2892ad8fb585
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-07-05 09:45:20 +0530

    create .env file and access it

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { Request, Response} from "express";
 import app from "./app";
-
+import dotenv from "dotenv";
+dotenv.config(); // load environment variables from .env
 
 // 1 initialize the express app
 /*const app : Express = express();*/
 
 // 2 define a application port
-const port = process.env.PORT || 3000;
+const port =
 
 // 3 define a simple http GET request
 app.get("/", (req : Request, res : Response) => {
+    console.log(req.body)
     res.send("Hello World!");
 });
 
